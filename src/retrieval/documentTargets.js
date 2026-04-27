7dc5f1e053b6f1ca5335e661a21b413a45fdd8ef
jashanschool <jashan.mahmoudi@icloud.com>
2026-04-27 19:49:22 +0200

    Update documentTargets.js

--- src/retrieval/documentTargets.js
+++ src/retrieval/documentTargets.js
@@ -1,1 +1,28 @@
+"use strict";
 
+const { normalizeAndValidateUrl } = require("./urlSafety");
+const { rankDocumentCandidates } = require("./candidateScoring");
+
+function extractDocumentTargets(links = [], options = {}) {
+  const candidates = [];
+
+  for (const link of links) {
+    const validation = normalizeAndValidateUrl(link.href, options);
+    if (!validation.ok) {
+      candidates.push({ ...link, usable: false, rejectionReason: validation.reason, score: 0 });
+      continue;
+    }
+
+    candidates.push({
+      ...link,
+      href: validation.url,
+      usable: true,
+    });
+  }
+
+  return rankDocumentCandidates(candidates);
+}
+
+module.exports = {
+  extractDocumentTargets,
+};
